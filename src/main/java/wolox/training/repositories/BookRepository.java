9c4f17e7af7d7205b019d97552a08207f1a830fa
Luis Miguel Rodriguez <luismiguelrodriguez@MacBook-Pro-de-Wolox.local>
2020-10-26 12:20:14 -0500

    wildcards are disabled and the method was modified

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wolox.training.models.Book;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
 
-    public Book findByAuthor(String author);
+    public Optional<Book> findByAuthor(String author);
 
 
 }
