d61284e62cd01f26b061f1e957c111ac1f20a84d
Luis Miguel Rodriguez <luismiguelrodriguez@MacBook-Pro-de-Wolox.local>
2020-10-26 16:24:37 -0500

    Add handle Exception and Add Users entity

---
+++ src/main/java/wolox/training/models/Users.java
@@ -0,0 +1,30 @@
+package wolox.training.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate birthdate;
+
+    @Column(nullable = false)
+    private List<Book> books;
+}
+
