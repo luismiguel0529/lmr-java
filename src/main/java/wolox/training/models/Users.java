ac18a43026906a775eb37c08c39a95f5c22caa27
Luis Miguel Rodriguez <luismiguelrodriguez@MacBook-Pro-de-Wolox.local>
2020-10-26 20:40:03 -0500

    Add Getter and Setter entity users

--- src/main/java/wolox/training/models/Users.java
+++ src/main/java/wolox/training/models/Users.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,15 +17,70 @@ public class Users {
     private Long id;
 
     @Column(nullable = false)
+    @NotEmpty
     private String userName;
 
     @Column(nullable = false)
+    @NotEmpty
     private String name;
 
     @Column(nullable = false)
+    @NotEmpty
     private LocalDate birthdate;
 
     @Column(nullable = false)
+    @NotEmpty
     private List<Book> books;
+
+    public Users() {
+    }
+
+    public Users(Long id, @NotEmpty String userName, @NotEmpty String name, @NotEmpty LocalDate birthdate, @NotEmpty List<Book> books) {
+        this.id = id;
+        this.userName = userName;
+        this.name = name;
+        this.birthdate = birthdate;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
 
