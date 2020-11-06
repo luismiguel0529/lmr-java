a117f73726cdcba81ff582d49ea41ae692cf3021
Luis Miguel Rodriguez <luismiguelrodriguez@MacBook-Pro-de-Wolox.local>
2020-11-06 10:04:54 -0500

    delete wildcard

--- src/test/java/wolox/training/controllers/BookControllerTest.java
+++ src/test/java/wolox/training/controllers/BookControllerTest.java
@@ -29,7 +29,10 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
